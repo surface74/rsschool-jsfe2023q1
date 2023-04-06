5453ebe69f90033cb014576afe64f0c114b8ea62
surface74 <warwhisper@tut.by>
2023-04-06 16:46:09 +0300

    fix: correct naming in script

--- shelter/src/js/index.js
+++ shelter/src/js/index.js
@@ -3,25 +3,20 @@
 const burger = document.querySelector('.burger');
 const headerNavbar = document.querySelector('.header__navbar');
 
-burger.addEventListener('click', toggleMenu);
-
-function toggleMenu(e) {
-  const menuSwitch = e.target;
-  console.dir(menuSwitch);
-
-  if (menuSwitch.classList.contains('burger_activated')) {
+burger.addEventListener('click', () => {
+  if (burger.classList.contains('burger_activated')) {
     hideMenu();
   } else {
     showMenu();
   }
-}
+});
 
 function hideMenu() {
-  menuSwitch.classList.remove('burger_activated');
+  burger.classList.remove('burger_activated');
   headerNavbar.classList.remove('header_menu-showed');
 }
 
 function showMenu() {
-  menuSwitch.classList.add('burger_activated');
+  burger.classList.add('burger_activated');
   headerNavbar.classList.add('header_menu-showed');
 }
