f9f933a8a2a533590c81d5980a69f674e96679c0
surface74 <warwhisper@tut.by>
2023-05-19 10:26:08 +0300

    feat: restyle Button

--- minesweeper/src/Button/index.js
+++ minesweeper/src/Button/index.js
@@ -2,11 +2,15 @@ import './index.scss';
 import ButtonHtml from './index.html';
 import HtmlHelper from '../utils/html-helper.js';
 
-const Button = ({ onClick, title, className }) => {
+const Button = ({ onClick = null, title, className = null }) => {
   const buttonElement = HtmlHelper.ElementFromHTML(ButtonHtml);
-  buttonElement.addEventListener('click', onClick);
+  if (onClick) {
+    buttonElement.addEventListener('click', onClick);
+  }
   buttonElement.innerHTML = title;
-  buttonElement.classList.add(className);
+  if (className) {
+    className.split(' ').forEach((c) => buttonElement.classList.add(c));
+  }
 
   return buttonElement;
 };
