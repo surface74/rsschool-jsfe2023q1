e4628ab024abb4738195e37ddd8fc1502c4a735f
surface74 <warwhisper@tut.by>
2023-05-19 21:02:15 +0300

    refactor: refactor Header to class

--- minesweeper/src/Header/index.js
+++ minesweeper/src/Header/index.js
@@ -3,11 +3,26 @@ import HeaderHtml from './index.html';
 import HtmlHelper from '../utils/html-helper.js';
 import Button from '../Button/index.js';
 
-const headerElement = HtmlHelper.ElementFromHTML(HeaderHtml);
-headerElement.append(Button({ title: 'New game', className: 'button-start' }));
-headerElement.append(Button({ title: 'Restore', className: 'button-restore' }));
-headerElement.append(Button({ title: 'Save', className: 'button-save' }));
-headerElement.append(Button({ title: 'Pause', className: 'button-pause' }));
-headerElement.append(Button({ title: 'Result', className: 'button-result' }));
+export default class Header {
+  constructor() {
+    this.element = HtmlHelper.ElementFromHTML(HeaderHtml);
+    this.buttonNewGame = Button({ title: 'New game', className: 'button-start' });
+    this.buttonRestore = Button({ title: 'Restore', className: 'button-restore' });
+    this.buttonSave = Button({ title: 'Save', className: 'button-save' });
+    this.buttonPause = Button({ title: 'Pause', className: 'button-pause' });
+    this.buttonResults = Button({ title: 'Top 10', className: 'button-results' });
+    this.init();
+  }
 
-export default headerElement;
+  init() {
+    this.element.append(this.buttonNewGame);
+    this.element.append(this.buttonRestore);
+    this.element.append(this.buttonSave);
+    this.element.append(this.buttonPause);
+    this.element.append(this.buttonResults);
+  }
+
+  getElement() {
+    return this.element;
+  }
+}
