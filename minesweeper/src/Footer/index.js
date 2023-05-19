8912bdbd5398c11be16c499bae411c33e7fd0f9b
surface74 <warwhisper@tut.by>
2023-05-19 14:36:20 +0300

    feat: add Footer component

--- minesweeper/src/Footer/index.js
+++ minesweeper/src/Footer/index.js
@@ -1,13 +1,9 @@
 import './index.scss';
 import FooterHtml from './index.html';
 import HtmlHelper from '../utils/html-helper.js';
-import Button from '../Button/index.js';
+import Sound from '../Sound/index.js';
 
 const footerElement = HtmlHelper.ElementFromHTML(FooterHtml);
-footerElement.append(Button({ title: 'New game', className: 'button-start' }));
-footerElement.append(Button({ title: 'Restore', className: 'button-restore' }));
-footerElement.append(Button({ title: 'Save', className: 'button-save' }));
-footerElement.append(Button({ title: 'Pause', className: 'button-pause' }));
-footerElement.append(Button({ title: 'Result', className: 'button-result' }));
+footerElement.append(Sound({}));
 
 export default footerElement;
