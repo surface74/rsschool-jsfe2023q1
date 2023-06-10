ddd7ca75d9e1921172dcf62239a10182787176d8
surface74 <warwhisper@tut.by>
2023-06-10 23:28:30 +0300

    feat: add API-key

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '27731bd001094f2691fd2b200ab13460', // получите свой ключ https://newsapi.org/
         });
     }
 }
