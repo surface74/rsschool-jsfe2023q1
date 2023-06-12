adacf340ea4c71e887ee3fb1bfaebc59ef62a293
surface74 <warwhisper@tut.by>
2023-06-12 11:56:07 +0300

    feat: add types to app.ts

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,17 +1,21 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
+import { IServerData, ISource } from '../../types';
 
 class App {
+    controller: AppController;
+    view: AppView;
+
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
     }
 
     start() {
-        document
-            .querySelector('.sources')
-            .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
-        this.controller.getSources((data) => this.view.drawSources(data));
+        (document.querySelector('.sources') as HTMLElement).addEventListener('click', (e: MouseEvent) =>
+            this.controller.getNews(e, (data: IServerData) => this.view.drawNews(data))
+        );
+        this.controller.getSources((data: ISource) => this.view.drawSources(data));
     }
 }
 
