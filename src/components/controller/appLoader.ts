37c4abb323815d50bc2b0642f216be9f7ccadfdb
surface74 <warwhisper@tut.by>
2023-06-13 01:38:31 +0300

    fix: add proxy-server name for production

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -3,6 +3,9 @@ import { DataType } from '../../types';
 
 class AppLoader extends Loader<DataType> {
     constructor() {
+        // super('https://rss-news-api.onrender.com/', {
+        //     apiKey: '27731bd001094f2691fd2b200ab13460', // получите свой ключ https://newsapi.org/
+        // });
         super('https://newsapi.org/v2/', {
             apiKey: '27731bd001094f2691fd2b200ab13460', // получите свой ключ https://newsapi.org/
         });
