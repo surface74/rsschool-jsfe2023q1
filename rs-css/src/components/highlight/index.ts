40ac9b0b7ea6053be5e0f53e328bc099fe816e90
surface74 <warwhisper@tut.by>
2023-07-06 11:09:57 +0300

    refactor: refine adaptive

--- rs-css/src/components/highlight/index.ts
+++ rs-css/src/components/highlight/index.ts
@@ -1,6 +1,8 @@
 import hljs from 'highlight.js/lib/core';
 import xml from 'highlight.js/lib/languages/xml';
 hljs.registerLanguage('xml', xml);
+import css from 'highlight.js/lib/languages/css';
+hljs.registerLanguage('css', css);
 import 'highlight.js/styles/vs2015.css';
 
 export default hljs;
